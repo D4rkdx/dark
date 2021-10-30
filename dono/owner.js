f4cd7e828144504f17635caf94ce928c1ef75a6d
D4rkdx <92230602+D4rkdx@users.noreply.github.com>
2021-10-30 11:04:54 -0500

    Add files via upload

---
+++ dono/owner.js
@@ -0,0 +1,16 @@
+const owner = (prefix, ownerNumber, NamaBot) => {
+	return`
+
+╔════ ◤ INFO DONO/BOT ◢
+║╔▸ 
+║╠ Nome :「 ${KING &QUEEN} 」
+║╠ Proprietário: wa.me/${+525552159127} 
+║╠ Youtube : 「 ${ytb} 」
+║╠ Instagram : ${instagram} 
+║╠ Prefix : ${prefix} 
+║╚▸
+╚══ ◤ *${DARKONE}* ◢
+`
+}
+
+exports.owner = owner
